1d05d7b066e806bd2ec4f928bc7df7bc49bf0506
Élio <1151557@isep.ipp.pt>
2020-01-06 22:15:43 +0000

    Updates

--- precache-manifest.a4167c57669924073962e394b481b547.js
+++ precache-manifest.a4167c57669924073962e394b481b547.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3eb2db3ab965a389ff78942c5523f272",
+    "revision": "173165ad61615ee3939fa1ff63886eb1",
     "url": "/Chamine037/index.html"
   },
   {
-    "revision": "d9cc2d85497617b70e78",
+    "revision": "e00d7c6ca5e09390e18f",
     "url": "/Chamine037/static/css/main.0aedc3f8.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Chamine037/static/js/2.06cb6517.chunk.js.LICENSE"
   },
   {
-    "revision": "d9cc2d85497617b70e78",
-    "url": "/Chamine037/static/js/main.3ca77cda.chunk.js"
+    "revision": "e00d7c6ca5e09390e18f",
+    "url": "/Chamine037/static/js/main.bda9453e.chunk.js"
   },
   {
     "revision": "35d4caa4f189ab4ff598",
